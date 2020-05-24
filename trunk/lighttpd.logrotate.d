6788ae1cc98ddde5a04c27b7b5c013c1557ba859
Nathan Owens <ndowens@artixlinux.org>
2020-05-24 09:23:10 -0500

    [trunk] -> [extra] 'lighttpd-1.4.55-1.1' add

--- trunk/lighttpd.logrotate.d
+++ trunk/lighttpd.logrotate.d
@@ -4,6 +4,8 @@
 	notifempty
 	sharedscripts
 	postrotate
-		systemctl reload lighttpd.service || true
+		[ -f /usr/bin/openrc ]	&& { rc-service lighttpd reload } || true
+		[ -f /usr/bin/runit ] && { sv restart lighttpd } || true
+		[ -f /usr/bin/s6-svc ] && { s6-rc -d lighttpd && s6-rc -u lighttpd } || true
 	endscript
 }
