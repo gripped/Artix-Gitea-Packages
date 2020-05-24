87106aadb044e8f1a115ca33029b70e158257f0c
Nathan Owens <ndowens@artixlinux.org>
2020-05-24 09:51:53 -0500

    [trunk] -> [extra] 'lighttpd-1.4.55-1.2' add

--- repos/extra-x86_64/lighttpd.logrotate.d
+++ repos/extra-x86_64/lighttpd.logrotate.d
@@ -6,6 +6,6 @@
 	postrotate
 		[ -f /usr/bin/openrc ]	&& { rc-service lighttpd reload } || true
 		[ -f /usr/bin/runit ] && { sv restart lighttpd } || true
-		[ -f /usr/bin/s6-svc ] && { s6-rc -d lighttpd && s6-rc -u lighttpd } || true
+		[ -f /usr/bin/s6-svc ] && { s6-rc -d lighttpd ; s6-rc -u lighttpd } || true
 	endscript
 }
